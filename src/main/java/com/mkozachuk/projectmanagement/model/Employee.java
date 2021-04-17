0d2c65c37acac9ac78cc4713c3b1f13fc605fe62
Maksym <maksym.kozachuk@protonmail.com>
2021-04-17 04:20:14 +0200

    Employee model implementation

--- src/main/java/com/mkozachuk/projectmanagement/model/Employee.java
+++ src/main/java/com/mkozachuk/projectmanagement/model/Employee.java
@@ -1,4 +1,126 @@
 package com.mkozachuk.projectmanagement.model;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String pesel;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "employees_projects",
+            joinColumns = {
+                    @JoinColumn(name = "employee_id", referencedColumnName = "id",
+                            nullable = false, updatable = false)},
+            inverseJoinColumns = {
+                    @JoinColumn(name = "project_id", referencedColumnName = "project_id",
+                            nullable = false, updatable = false)})
+    private Set<Project> projects = new HashSet<>();
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String email, String pesel) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.pesel = pesel;
+    }
+
+    public Employee(String firstName, String lastName, String email, String pesel, Set<Project> projects) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.pesel = pesel;
+        this.projects = projects;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(employeeId, employee.employeeId) &&
+                Objects.equals(firstName, employee.firstName) &&
+                Objects.equals(lastName, employee.lastName) &&
+                Objects.equals(email, employee.email) &&
+                Objects.equals(pesel, employee.pesel) &&
+                Objects.equals(projects, employee.projects);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employeeId, firstName, lastName, email, pesel, projects);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", pesel='" + pesel + '\'' +
+                ", projects=" + projects +
+                '}';
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public Set<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set<Project> projects) {
+        this.projects = projects;
+    }
 }
